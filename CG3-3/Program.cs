96dbafd07df9b941902ac0a25dcda51b650c5568
KendallU <kendallunion@gmail.com>
2018-07-29 17:11:02 -0500

    Add project files.

---
+++ CG3-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CG3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Statments- A line of that execute an action
+             */
+
+            Console.WriteLine("Statement!");
+
+            /* Expression- Code that contain operators and operands
+             */
+            int x = 5;
+            int y = 2;
+            int z = x + y;
+            /* (x + y) is the expression
+             */
+
+            /* Operator- what takes place tp the operands in an expression
+             */
+
+            int a = 5;
+            int b = 2;
+            int c = a + b;
+            /* "+" is the operator
+             */
+
+            /* Operand- the variables or the main object in an expression
+             */
+
+            int d = 5;
+            int e = 2;
+            int f = d + e;
+            /* d and e are the operands
+             */
+
+                
+        }
+    }
+}
